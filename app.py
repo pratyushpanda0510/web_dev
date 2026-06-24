1884d2efb76dfb59e665338a3fdca73a9010b91f
pratyushpanda0510 <pratyush.panda0510@gmail.com>
2026-06-24 12:33:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("My first site")
+st.subheader("Welcome to my site")
+st.radio("Enter your gender", ["gay","lesbian"])
+st.video("hell_naw.mp4")
+st.write("Thankyou for your input")
